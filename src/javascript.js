996cae3597a339ac2bbb035fa0ae5c6d93751b75
Atho1218 <125186173+Atho1218@users.noreply.github.com>
2025-02-12 22:26:57 +0200

    Added js elements

--- src/javascript.js
+++ src/javascript.js
@@ -1,9 +1,27 @@
+function resultsDisplayed(response) {
+  temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "60at455d3d23aedc50eab463bf1f843o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(resultsDisplayed);
+}
+
 function displayTemperature(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input-form");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayTemperature);
+
+searchCity("Lisbon");
