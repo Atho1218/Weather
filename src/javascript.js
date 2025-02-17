8ecfb33f94570dcd64ccc6cfac8d2de842af6e76
Atho1218 <125186173+Atho1218@users.noreply.github.com>
2025-02-18 00:42:47 +0200

    fixed the days

--- src/javascript.js
+++ src/javascript.js
@@ -55,6 +55,14 @@ function displayTemperature(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "60at455d3d23aedc50eab463bf1f843o";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
@@ -64,22 +72,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `  
-      <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `  
+      <div >
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+           
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
             <div class="weather-forecast-temperatures">
-              <strong>17°</strong>
-              <div class="weather-forecast-temperature">15°</div>
+               <div class="weather-forecast-temperature">
+
+                <strong>${Math.round(day.temperature.maximum)}º</strong>
+              </div>
+                <div class="weather-forecast-temperature">${Math.round(
+                  day.temperature.minimum
+                )}°</div>
             </div>
           </div>
           `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
